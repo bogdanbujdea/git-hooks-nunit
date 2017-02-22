188f4160d7f8b4abe6bc1b987451d9bbd6f36aba
Bogdan Bujdea <bogdan@thewindev.net>
2017-02-22 11:47:53 +0200

    Added unit test

---
+++ HookTests.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace HookTests.Tests
+{
+    public class Class1
+    {
+        [Test]
+        public void test_s()
+        {
+            Calculator.Add(1, 2).Should().Be(3);
+        }
+
+        [Test]
+        public void test_f()
+        {
+            Calculator.Add(1, 2).Should().Be(4);
+        }
+    }
+}
